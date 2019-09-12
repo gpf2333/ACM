08c88d5b61d75982303024f1b792f8fd205de691
2019ZSS <1033741293@qq.com>
2019-09-12 20:15:25 +0700

    update

---
+++ 1 数学/1.1 素数/线性素数筛.cpp
@@ -0,0 +1,22 @@
+#include<bits/stdc++.h>
+using namespace std;
+const int maxn = 1e6 + 10;
+bool isprime[maxn];
+int cntp, prime[maxn];
+void init(){
+    memset(isprime, 1, sizeof(isprime));
+    cntp = isprime[0] = isprime[1] = 0;
+    for(int i = 2; i < maxn; ++i){
+        if(isprime[i]){
+            prime[++cntp] = i;
+        }
+        for(int j = 1; j <= cntp && i * prime[j] < maxn; ++j){
+            isprime[i * prime[j]] = 0;
+            if(i % prime[j] == 0) break ;
+        }
+    }
+}
+int main(){
+    init();
+    return 0;
+}
